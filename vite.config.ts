7f03067fdba74d503f4a332f6f65f084adec1976
Elina Razina <e.razina@samolet.ru>
2025-01-29 18:57:23 +0700

    new build

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,9 @@ const __dirname = path.dirname(new URL(import.meta.url).pathname);
 export default defineConfig({
   plugins: [react()],
   base: "/cryptoCurrencyApp/",
+  build: {
+    outDir: "dist",
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
